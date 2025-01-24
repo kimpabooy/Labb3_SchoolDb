f7372a13c6c88b4a3922a1cf3e0eb055b9d49188
Kim Andersson <Kimpabooy@live.com>
2025-01-24 08:32:26 +0100

    Add project files.

---
+++ Labb3_SchoolDb/Models/Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb3_SchoolDb.Models
+{
+    public class Class
+    {
+        [Key]
+        public int ClassId { get; set; }
+        public string Name { get; set; }
+        public ICollection Staffs { get; set; }
+        public ICollection Students { get; set; }
+
+        [ForeignKey("StaffId")]
+        public int StaffId { get; set; }
+        public Staff Staff { get; set; }
+
+    }
+}
---
+++ Labb3_SchoolDb/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Labb3_SchoolDb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
