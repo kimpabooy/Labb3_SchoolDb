a3db7da018f673ba507a9f7e5b0ec76ef79ecd94
Kim Andersson <Kimpabooy@live.com>
2025-01-27 07:34:19 +0100

    Fixed the method GetAllStudentsInAClass()

--- Labb3_SchoolDb/DatabaseManager.cs
+++ Labb3_SchoolDb/DatabaseManager.cs
@@ -1,4 +1,6 @@
 ﻿using Labb3_SchoolDb.Data;
+using Labb3_SchoolDb.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,26 +28,42 @@ namespace Labb3_SchoolDb
                 }
             }
         }
+
         public void GetAllStudentsInAClass()
         {
             using (var context = new SchoolDbContext())
             {
-                var showClass = context.Classes;
-
-                foreach (var item in context.Classes)
+                // Hämta alla klasser och skriv ut deras namn
+                var classes = context.Classes.ToList();
+                foreach (var item in classes)
                 {
-                    Console.WriteLine($"{}. {item.Name}");
+                    Console.WriteLine($"{item.Name}");
+                }
+
+                Console.WriteLine("Vilken klass vill du kolla på?");
+                Console.Write("Svar: ");
+                var userChoice = Console.ReadLine().ToUpper();
+
+                // Hämtar den valda klassen med dess elever
+                var activeClass = context.Classes
+                    .Include(c => c.Students)
+                    .FirstOrDefault(c => c.Name.ToUpper() == userChoice);
+
+                if (activeClass != null)
+                {
+                    Console.WriteLine($"Elever i {activeClass.Name}:");
+                    foreach (var student in activeClass.Students)
+                    {
+                        Console.WriteLine($"{student.FirstName} {student.LastName}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ingen klass hittad");
                 }
             }
-            Console.WriteLine("Vilken class vill du kolla på?");
-            var userChoice = Console.ReadLine();
-            
-            //switch (userChoice)
-            //{
-            //    default:
-            //}
         }
-
+       
         public void AddStaff()
         {
             using(var context = new SchoolDbContext())
